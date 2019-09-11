303e44d900b93641058d2c87a841097307821ad3
Corentin D <46219142+monsieurPaon@users.noreply.github.com>
2019-09-11 13:16:21 -0400

    Update to return 200

--- checker.sh
+++ checker.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+#!/bin/sh
+
 while read line
 do
   url=(`echo ${line}`)
   status=`curl -LI "${url}" -o /dev/null -w '%{http_code}\n' -s`
   if [ ${status} -eq 404 ]; then
-    echo "${status} ${url}"
-  fi
+  echo "${status} ${url}"
+elif [ ${status} -eq 200 ]; then
+   echo "${status} ${url}"
+fi
 done < "urls"
+
