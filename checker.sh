eb65ead71a2d51c6327c391032f5d3f3ffca7aef
Corentin D <46219142+monsieurPaon@users.noreply.github.com>
2019-09-11 13:37:16 -0400

    Enable write to csv

--- checker.sh
+++ checker.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
+DESTINATION='url-check-results.csv'
 
 while read line
 do
   url=(`echo ${line}`)
   status=`curl -LI "${url}" -o /dev/null -w '%{http_code}\n' -s`
   if [ ${status} -eq 404 ]; then
-  echo "${status} ${url}"
+  echo "${status}, ${url}" >> $DESTINATION
 elif [ ${status} -eq 200 ]; then
-   echo "${status} ${url}"
+   echo "${status}, ${url}" >> $DESTINATION
 fi
 done < "urls"
-
